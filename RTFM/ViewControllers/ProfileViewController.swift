d520d4eff7edd4f1993300ca0bbdaa6ae2aec254
Evgeny Bogomolov <beastgrim@gmail.com>
2019-09-28 20:40:47 +0300

    Profile UI

--- RTFM/ViewControllers/ProfileViewController.swift
+++ RTFM/ViewControllers/ProfileViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    private(set) var bgImageView: UIImageView!
+    private(set) var avaImageView: UIImageView!
+    private(set) var nameLabel: UILabel!
+    private(set) var phoneLabel: UILabel!
+    private(set) var bonusLabel: UILabel!
+    private(set) var useButton: UIButton!
+
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -20,19 +27,75 @@ class ProfileViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        self.bgImageView = UIImageView(image: UIImage(named: "profile-back"))
+        self.bgImageView.contentMode = .scaleAspectFill
+        self.view.addSubview(self.bgImageView)
+        self.bgImageView.snp.makeConstraints { (make) in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
 
-        // Do any additional setup after loading the view.
+        self.avaImageView = UIImageView(image: UIImage(named: "ava-axample"))
+        self.avaImageView.layer.cornerRadius = 60
+        self.avaImageView.clipsToBounds = true
+        self.view.addSubview(self.avaImageView)
+        self.avaImageView.snp.makeConstraints { (make) in
+            make.width.equalTo(120)
+            make.height.equalTo(120)
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview().offset(150)
+        }
+        
+        self.nameLabel = UILabel()
+        self.nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .medium)
+        self.nameLabel.textColor = .white
+        self.nameLabel.text = "Маша Иванова"
+        self.view.addSubview(self.nameLabel)
+        self.nameLabel.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.left.greaterThanOrEqualTo(self.view.snp.left).offset(32)
+            make.right.lessThanOrEqualTo(self.view.snp.right).offset(-32)
+            make.top.equalTo(self.avaImageView.snp.bottom).offset(50)
+        }
+        
+        self.phoneLabel = UILabel()
+        self.phoneLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        self.phoneLabel.textColor = .white
+        self.phoneLabel.text = "+7 945 835 82 56"
+        self.view.addSubview(self.phoneLabel)
+        self.phoneLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self.nameLabel.snp.bottom).offset(16)
+            make.left.greaterThanOrEqualTo(self.view.snp.left).offset(32)
+            make.right.lessThanOrEqualTo(self.view.snp.right).offset(-32)
+            make.centerX.equalToSuperview()
+        }
+        
+        self.bonusLabel = UILabel()
+        self.bonusLabel.font = UIFont.systemFont(ofSize: 30, weight: .medium)
+        self.bonusLabel.textColor = rgba(43, 34, 118, 0.9)
+        self.bonusLabel.text = "530 Б"
+        self.view.addSubview(self.bonusLabel)
+        self.bonusLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self.phoneLabel.snp.bottom).offset(24)
+            make.centerX.equalToSuperview()
+        }
+        
+        self.useButton = UIButton.makeSystemButton()
+        self.useButton.setTitle("ИСПОЛЬЗОВАТЬ", for: .normal)
+        self.useButton.addTarget(self, action: #selector(actionUseBonus(_:)), for: .touchUpInside)
+        self.view.addSubview(self.useButton)
+        self.useButton.snp.makeConstraints { (make) in
+            make.top.equalTo(self.bonusLabel.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc
+    func actionUseBonus(_ sender: Any?) {
+        // TODO: Show variants
     }
-    */
-
 }
